7de0c26906174f3edf7e9702473ca479ba48bdf1
BuenosVictor <buenos.victor2004@gmail.com>
2024-11-04 07:37:05 -0300

    chore: configura React Router DOM nos arquivos App.js e index.js
    - Criadas as rotas no arquivo index.js e configuradas usando o componente Outlet, permitindo que os componentes mudem conforme a navegação.
    - Ajustados os componentes no arquivo App.js para garantir que tudo funcione bem com as rotas.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './index.css';
 import { Header } from './Header-component/Header.js'
-import { Introduction } from './Introduction-component/Introduction.js'
-import { ProjectsCarousel } from './ProjectsCarousel-component/ProjectsCarousel.js';
-import { SkillsAndAboutMe } from './SkillsAndAboutMe-component/SkillsAndAboutMe.js';
 import { Footer } from './Footer-component/Footer.js';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
 
+    
 
 
     <div className="App">
+
       <Header />
-      <Introduction />
-      <ProjectsCarousel />
-      <SkillsAndAboutMe />
+      <Outlet />
       <Footer />
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Introduction } from './Introduction-component/Introduction.js'
+import { ProjectsCarousel } from './ProjectsCarousel-component/ProjectsCarousel.js';
+import { SkillsAndAboutMe } from './SkillsAndAboutMe-component/SkillsAndAboutMe.js';
+import { PageAboutMe } from './PageAboutMe-component/PageAboutMe.js';
+import { ProjectsPage } from './ProjectsPage-component/ProjectsPage.js';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element:
+          <>
+            <Introduction />
+            <ProjectsCarousel />
+            <SkillsAndAboutMe />
+          </>
+      },
+      {
+        path:"Sobre mim",
+        element:<PageAboutMe/>,
+      },
+      {
+        path:"Projetos",
+        element:<ProjectsPage/>,
+      }
+    ]
+
+  },
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
