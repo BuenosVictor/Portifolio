3946611123dd7e22e4c1e662b19c507b20888639
BuenosVictor <buenos.victor2004@gmail.com>
2024-11-04 07:20:04 -0300

    feat: cria componente de página para exibir todos os projetos
    - Cria um novo componente responsável por listar e mostrar todos os projetos desenvolvidos até o momento.
    - A página apresenta uma visão geral dos projetos, permitindo que os usuários vejam rapidamente o trabalho realizado.
    - A estrutura da página criada é inicial e serve como um ponto de partida para futuras implementações e melhorias.

---
+++ src/ProjectsPage-component/ProjectsPage.js
@@ -0,0 +1,122 @@
+import '../index.css'
+import HTML from './HTML.svg'
+import CSS from './CSS.svg'
+import Javascript from './Javascript.svg'
+import WheatherProject from './Weather.svg'
+import PokemonProject from './Pokemon.svg'
+import OnePieceProject from './OnePiece.svg'
+import MemoryProject from './Memory.svg'
+
+export function ProjectsPage() {
+
+
+    return (
+
+        <section className='pt-24 toFit flex flex-col gap-10'>
+            <article className='flex py-32 items-center justify-around bg-white rounded-3xl drop-shadow-2xl'>
+                <img className='w-1/4' src={WheatherProject} />
+
+                <div className='flex flex-col items-center gap-7 text-center'>
+                    <h3>App para o Clima</h3>
+                    <p>Digite o nome da cidade desejada na barra de pesquisa e tenha acesso ao clima, velocidade do vento e umidade em suas mãos!</p>
+                    <h3>Tecnologias:</h3>
+                    <div className='w-fit gap-x-24 grid grid-cols-3 grid-rows-2 justify-items-center'>
+                        <img className='row-start-1 w-12' src={HTML} />
+                        <p className='text-xl'>HTML</p>
+
+                        <img className='row-start-1 w-12' src={CSS} />
+                        <p className='text-xl'>CSS</p>
+
+                        <img className='row-start-1 w-12' src={Javascript} />
+                        <p className='text-xl'>Javascript</p>
+                    </div>
+                </div>
+            </article>
+
+            <article className='flex py-32 items-center justify-around bg-white rounded-3xl drop-shadow-2xl'>
+
+                <div className='flex flex-col items-center gap-7 text-center'>
+                    <h3>App para o Clima</h3>
+                    <p>Digite o nome da cidade desejada na barra de pesquisa e tenha acesso ao clima, velocidade do vento e umidade em suas mãos!</p>
+                    <h3>Tecnologias:</h3>
+                    <div className='w-fit gap-x-24 grid grid-cols-3 grid-rows-2 justify-items-center'>
+                        <img className='row-start-1 w-12' src={HTML} />
+                        <p className='text-xl'>HTML</p>
+
+                        <img className='row-start-1 w-12' src={CSS} />
+                        <p className='text-xl'>CSS</p>
+
+                        <img className='row-start-1 w-12' src={Javascript} />
+                        <p className='text-xl'>Javascript</p>
+                    </div>
+                </div>
+
+                <img src={MemoryProject} />
+            </article>
+
+            <article className='flex py-32 items-center justify-around bg-white rounded-3xl drop-shadow-2xl'>
+                <img src={OnePieceProject} />
+
+                <div className='flex flex-col items-center gap-7 text-center'>
+                    <h3>App para o Clima</h3>
+                    <p>Digite o nome da cidade desejada na barra de pesquisa e tenha acesso ao clima, velocidade do vento e umidade em suas mãos!</p>
+                    <h3>Tecnologias:</h3>
+                    <div className='w-fit gap-x-24 grid grid-cols-3 grid-rows-2 justify-items-center'>
+                        <img className='row-start-1 w-12' src={HTML} />
+                        <p className='text-xl'>HTML</p>
+
+                        <img className='row-start-1 w-12' src={CSS} />
+                        <p className='text-xl'>CSS</p>
+
+                        <img className='row-start-1 w-12' src={Javascript} />
+                        <p className='text-xl'>Javascript</p>
+                    </div>
+                </div>
+
+            </article>
+
+            <article className='flex py-32 items-center justify-around bg-white rounded-3xl drop-shadow-2xl'>
+
+            <div>
+                <h3>App para o Clima</h3>
+                <p>Digite o nome da cidade desejada na barra de pesquisa e tenha acesso ao clima, velocidade do vento e umidade em suas mãos!</p>
+                <h3>Tecnologias:</h3>
+                <div className='w-fit gap-x-24 grid grid-cols-3 grid-rows-2 justify-items-center'>
+                    <img className='row-start-1 w-12' src={HTML} />
+                    <p className='text-xl'>HTML</p>
+
+                    <img className='row-start-1 w-12' src={CSS} />
+                    <p className='text-xl'>CSS</p>
+
+                    <img className='row-start-1 w-12' src={Javascript} />
+                    <p className='text-xl'>Javascript</p>
+                </div>
+            </div>
+
+            <img src={PokemonProject} />
+            </article>
+
+            <article className='flex py-32 mb-28 items-center justify-around bg-white rounded-3xl drop-shadow-2xl'>
+                <img className='w-1/4' src={WheatherProject} />
+
+                <div className='flex flex-col items-center gap-7 text-center'>
+                    <h3>App para o Clima</h3>
+                    <p>Digite o nome da cidade desejada na barra de pesquisa e tenha acesso ao clima, velocidade do vento e umidade em suas mãos!</p>
+                    <h3>Tecnologias:</h3>
+                    <div className='w-fit gap-x-24 grid grid-cols-3 grid-rows-2 justify-items-center'>
+                        <img className='row-start-1 w-12' src={HTML} />
+                        <p className='text-xl'>HTML</p>
+
+                        <img className='row-start-1 w-12' src={CSS} />
+                        <p className='text-xl'>CSS</p>
+
+                        <img className='row-start-1 w-12' src={Javascript} />
+                        <p className='text-xl'>Javascript</p>
+                    </div>
+                </div>
+            </article>
+
+        </section >
+
+    )
+}
