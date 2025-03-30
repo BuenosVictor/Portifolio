be2d8d770b3df599eccb44c35026b394598efd2e
BuenosVictor <buenos.victor2004@gmail.com>
2025-03-30 15:53:04 -0300

    Melhorias na organização e transições do Header
    - Adicionadas animações para suavizar interações.
    - Melhorada centralização e organização dos elementos.
    - Implementada borda e hover apenas no container do nome e cargo.

--- src/Header-component/Header.js
+++ src/Header-component/Header.js
@@ -2,43 +2,37 @@ import '../index.css'
 import { Link } from 'react-router-dom';
 
 export function Header() {
-
-
     return (
-        <header className='p-2 rounded-md bg-white bg-opacity-50 backdrop-blur-sm fixed left-[5vw] w-full z-50 flex justify-between items-center top-6 toFit'>
-
-            <div className='flex flex-col items-center'>
+        <header className='p-2 rounded-md bg-white bg-opacity-50 backdrop-blur-sm fixed left-[5vw] w-full z-50 flex justify-between items-center top-6 toFit transition duration-300'>
+            <div className='p-1 flex flex-col items-center text-center duration-300 rounded-md border-2 border-transparent hover:border-black'>
                 <Link to="/">
                     <h1 className='font-bold text-xl'>Victor de Souza</h1>
                     <h2 className='bg-primaryL text-white text-sm p-1 rounded-md font-bold'>Desenvolvedor Frontend</h2>
                 </Link>
             </div>
 
-
-            <nav class="dropdownmenu">
+            <nav className="dropdownmenu">
                 <ul>
-                    <li>
+                    <li className='duration-300'>
                         <Link to="/" className='rounded-md'>Home</Link>
                     </li>
-                    <li >
+                    <li className='transition duration-300'>
                         <a className='rounded-t-md ' href="#SkillsAndAboutMe">Sobre Mim</a>
-                        <ul className="submenu">
-                            <li><Link to="Sobre mim" className=' rounded-b-md'>Detalhes</Link></li>
+                        <ul className="submenu transition duration-300">
+                            <li><Link to="Sobre mim" className='rounded-b-md'>Detalhes</Link></li>
                         </ul>
                     </li>
-                    <li>
+                    <li className='transition duration-300'>
                         <a className='rounded-t-md' href="#ProjectsCarousel">Projetos</a>
-                        <ul className="submenu">
+                        <ul className="submenu transition duration-300">
                             <li> <Link to="Projetos" className='rounded-b-md'>Explorar</Link></li>
                         </ul>
                     </li>
-                    <li>
+                    <li className='duration-300'>
                         <a className='rounded-md' href="#Footer">Contatos</a>
                     </li>
                 </ul>
             </nav>
-
         </header>
     );
-
 }
